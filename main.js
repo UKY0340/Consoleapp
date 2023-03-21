31cfff016740b4ccc65d73afe1c04a956951a9f5
UKY0340 <96613831+UKY0340@users.noreply.github.com>
2023-03-21 16:16:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+  console.log("my first console project");
+var first_name ="Ranvir";
+var last_name = "kapoor";
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+var a = 10;
+var b = 5;
+var add = a + b;
+console.log(subtract);
+var multiply = a * b;
+console.log(multiply);
+var divide = a/ b;
+console.log(divide);
+
+
+
+            
